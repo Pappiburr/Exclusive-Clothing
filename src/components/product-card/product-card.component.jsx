8fbb9ace724d4c5891259c865b524f717942fe32
Alexander Perez <116762210+Pappiburr@users.noreply.github.com>
2024-12-10 14:01:23 -0600

    Added Cart Context

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,11 @@
 import './product-card.styles.scss';
 import Button from '../button/button.component';
+import {CartContext} from '../contexts/cart.context';
+import { useContext } from 'react';
 
 const ProductCard = ({product}) => {
     const {name, price, imageUrl} = product;
+    const {addCartItem} = useContext(CartContext);
     return(
     <div className="product-card-container">
         <img src={imageUrl} alt={`${name}`}/>
@@ -10,7 +13,7 @@ const ProductCard = ({product}) => {
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
-    <Button buttonType="inverted">Add To Cart</Button>
+    <Button buttonType="inverted" onClick={addCartItem}>Add To Cart</Button>
     </div>
     );
 
