5f83a2ca517af75da962e4f0d7fbcf6f92aa2f74
Alexander Perez <116762210+Pappiburr@users.noreply.github.com>
2024-12-19 13:50:19 -0600

    Categories Reducer Set Up
    Set up the base for the categories Reducer.

---
+++ src/store/categories/categories.action.js
@@ -0,0 +1,5 @@
+import { CATEGORIES_ACTION_TYPES } from './categories.types';
+import { createAction } from '../../utils/reducer/reducer.utils';
+
+export const setCategories = (categoriesMap) =>
+     createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP, categoriesMap);
