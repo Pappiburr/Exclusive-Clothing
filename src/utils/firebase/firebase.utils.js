83be15cae21d98f61a0dfef9cfc4772374ecaa57
Alexander Perez <116762210+Pappiburr@users.noreply.github.com>
2025-01-07 16:01:25 -0600

    Updated reducer
    switched reducer from firebase utils to be used by the category selector.

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -63,13 +63,7 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
     const collectionRef = collection(db, 'categories');
     const q = query(collectionRef);
     const querySnapshot = await getDocs(q);
-    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-        const {title, items} = docSnapshot.data();
-        acc[title.toLowerCase()] = items;
-        return acc;
-    }, {});
-    return categoryMap;
-  }
+    return querySnapshot.docs.map(docSnapshot => docSnapshot.data());}
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
     if(!userAuth) return;
