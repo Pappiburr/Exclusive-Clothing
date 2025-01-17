699c024a286752cc57360366c7c618d8c757127b
Alexander Perez <116762210+Pappiburr@users.noreply.github.com>
2025-01-17 16:21:54 -0600

    Redux Persist
    Added Redux persist to maintain cart throughout load

--- src/store/store.js
+++ src/store/store.js
@@ -1,33 +1,45 @@
 import {legacy_createStore as createStore, compose, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
 
 
-const loggerMiddlerware = ( store ) => (next) => (action) => {
-  if(!action.type){
-    return next()
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action) {
+    return next(action);
   }
 
-  console.log('type' , action.type);
+  console.log('type', action.type);
   console.log('payload', action.payload);
   console.log('currentState', store.getState());
 
-  next(action);
+  const result = next(action);
 
+  console.log('next state:', store.getState());
 
-  console.log('next state:' , store.getState());
-
-
+  return result;
 }
-const middleWares = [loggerMiddlerware];
+
+const persistConfig = {
+  key: 'root', 
+  storage,
+  blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const middleWares = [loggerMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middleWares.push(logger);
 }
 
 export const store = createStore(
-  rootReducer, 
-  undefined, 
+  persistedReducer,
+  undefined,
   compose(applyMiddleware(...middleWares))
 );
+
+export const persistor = persistStore(store);
